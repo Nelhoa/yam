0ce7318271ea21d6b5dac18d2fd53e9a988dd185
Neil Savin <savin.neil.56@gmail.com>
2024-01-12 17:37:42 +0100

    cleaning

--- src/lib/stores/formulas-loading.ts
+++
@@ -1,3 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const formulasLoadingStore = writable<boolean>(false);
--- src/lib/stores/settings-store.ts
+++
@@ -1,5 +0,0 @@
-import type { DateRange } from '$lib/types/customTypes';
-import { rangeTemplates } from '$lib/utils/Public/Helpers/range-template';
-import { writable } from 'svelte/store';
-
-export const global_date_range = writable<DateRange>(rangeTemplates.OneYear);
