77e4673d4d89470d4573b51175e1a1648dd2bdab
Neil Savin <savin.neil.56@gmail.com>
2025-09-09 19:27:33 +0200

    qr code improve style

--- src/routes/qr-code/+page.svelte
+++ src/routes/qr-code/+page.svelte
@@ -36,19 +36,19 @@
 	<h1 class="mb-5 text-xl font-bold">QR Code</h1>
 	<!-- <div class="mb-1 text-sm font-semibold">Votre URL</div> -->
 	<input
-		class="w-full rounded border border-black/10 px-3 py-1"
+		class="w-full rounded border border-black/10 px-3 py-1 selection:bg-cyan-200 focus:outline-cyan-400"
 		type="text"
 		bind:value={url}
 		on:input={generate}
 		placeholder="Entrez une URL"
 	/>
 	<!-- affichage du SVG -->
-	<div class="w-full py-8">
+	<div class="my-8 w-full rounded border bg-cyan-50 p-8">
 		{@html svgCode}
 	</div>
 
 	<button
-		class="rounded bg-cyan-500 px-3 py-1 font-semibold text-white hover:bg-cyan-600"
+		class="rounded bg-cyan-500 px-3 py-1 font-semibold text-white hover:bg-cyan-600 focus:outline-cyan-700"
 		on:click={download}>Télécharger (SVG)</button
 	>
 </div>
